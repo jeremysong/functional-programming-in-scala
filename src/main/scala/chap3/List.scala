03f03558237aeea68b80606c5aedc608619a94b0
Jeremy Song <jeremy.song@me.com>
2020-11-21 12:42:23 -0800

    Add solutions to exercises in Chap 3.3

--- src/main/scala/chap3/List.scala
+++ src/main/scala/chap3/List.scala
@@ -51,4 +51,73 @@ object List {
       case Cons(x, xs) => drop(xs, n - 1)
     }
   }
+
+  /**
+   * Exercise 3.5
+   */
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => if (f(x)) xs else Cons(x, dropWhile(xs, f))
+  }
+
+  /**
+   * Exercise 3.6
+   */
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  def sum2(ns: List[Int]): Int = foldRight(ns, 0)(_ + _)
+
+  def product2(ns: List[Double]): Double = foldRight(ns, 1.0)(_ * _)
+
+  /** Exercise 3.9 */
+  def length[A](as: List[A]): Int = foldRight(as, 0)((_, y) => 1 + y)
+
+  /** Exercise 3.10 */
+  @tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
+
+  def length2[A](as: List[A]): Int = foldLeft(as, 0)((x, _) => x + 1)
+
+  /** Exercise 3.11 */
+  def sum3(ns: List[Int]): Int = foldLeft(ns, 0)(_ + _)
+
+  def product3(ns: List[Double]): Double = foldLeft(ns, 1.0)(_ * _)
+
+  /** Exercise 3.12 */
+  def reverse[A](ns: List[A]): List[A] = ns match {
+    case Nil => ns
+    case Cons(x, xs) => foldLeft(xs, List(x))((l, x) => Cons(x, l))
+  }
+
+  /** Exercise 3.13 */
+  def foldLeft2[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
+    foldRight(as, z)((a, b) => f(b, a))
+  }
+
+  def foldRight2[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
+    foldLeft(as, z)((a, b) => f(b, a))
+  }
+
+  /** Exercise 3.14 */
+  def append[A](as: List[A], a: A): List[A] = {
+    foldRight(as, List(a))((b, l) => Cons(b, l))
+  }
+
+  /** Exercise 3.15 */
+  def concat[A](as: List[List[A]]): List[A] = as match {
+    case Nil => Nil
+    case Cons(x, xs) => foldLeft(xs, x)((l1, l2) => foldRight(l1, l2)((a, b) => Cons(a, b)))
+  }
 }
--- src/test/scala/chap3/ListTest.scala
+++ src/test/scala/chap3/ListTest.scala
@@ -1,8 +1,10 @@
 package chap3
 
 import org.scalatest.flatspec.AnyFlatSpec
-import chap3.List._
+import chap3.List.{length, _}
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+import scala.annotation.tailrec
 
 class ListTest extends AnyFlatSpec {
   behavior of "List"
@@ -37,5 +39,70 @@ class ListTest extends AnyFlatSpec {
     drop(List(1, 2, 3), 0) shouldEqual List(1, 2, 3)
     drop(List(1, 2, 3), 1) shouldEqual List(2, 3)
     drop(List(1, 2, 3), 4) shouldEqual Nil
+  }
+
+  it should "dropWhile correct items" in {
+    val predicate = (x: Int) => x == 3
+    dropWhile(Nil, predicate) shouldEqual Nil
+    dropWhile(List(1, 2, 3), predicate) shouldEqual List(1, 2)
+    dropWhile(List(1, 2, 4), predicate) shouldEqual List(1, 2, 4)
+  }
+
+  it should "init correct items" in {
+    init(Nil) shouldEqual Nil
+    init(List(1, 2, 3)) shouldEqual List(1, 2)
+  }
+
+  it should "calculate correct length using foldRight" in {
+    length(Nil) shouldEqual 0
+    length(List(1)) shouldEqual 1
+    length(List(1, 2, 3)) shouldEqual 3
+  }
+
+  it should "calculate correct length using foldLeft" in {
+    length2(Nil) shouldEqual 0
+    length2(List(1)) shouldEqual 1
+    length2(List(1, 2, 3)) shouldEqual 3
+  }
+
+  it should "calculate correct results using sum3 and product3" in {
+    sum3(Nil) shouldEqual 0
+    sum3(List(1, 2)) shouldEqual 3
+
+    product3(Nil) shouldEqual 1.0
+    product3(List(1, 3)) shouldEqual 3.0
+  }
+
+  it should "reverse a list" in {
+    reverse(Nil) shouldEqual Nil
+    reverse(List(1, 2)) shouldEqual List(2, 1)
+    reverse(List(1, 2, 3)) shouldEqual List(3, 2, 1)
+  }
+
+  it should "calculate correct length using foldLeft2" in {
+    def length[A](as: List[A]): Int = foldLeft2(as, 0)((x, _) => x + 1)
+
+    length(Nil) shouldEqual 0
+    length(List(1)) shouldEqual 1
+    length(List(1, 2, 3)) shouldEqual 3
+  }
+
+  it should "calculate correct length using foldRight2" in {
+    def length[A](as: List[A]): Int = foldRight2(as, 0)((_,y) => y + 1)
+
+    length(Nil) shouldEqual 0
+    length(List(1)) shouldEqual 1
+    length(List(1, 2, 3)) shouldEqual 3
+  }
+
+  it should "append correct item to list" in {
+    append(Nil, 1) shouldEqual List(1)
+    append(List(1, 2), 3) shouldEqual List(1, 2, 3)
+  }
+
+  it should "concat two list correctly" in {
+    concat(Nil) shouldEqual Nil
+    concat(List(List(1), List(2))) shouldEqual List(1, 2)
+    concat(List(List(1, 2), List(3, 4), List(5, 6))) shouldEqual List(1, 2, 3, 4, 5, 6)
   }
 }
