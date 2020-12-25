650ce76d83e566b61da6a305c679554a9e9a99e1
Jeremy Song <jeremy.song@me.com>
2020-12-24 21:28:36 -0800

    Complete exercises in Chapter 5.3

--- src/main/scala/chap5/Stream.scala
+++ src/main/scala/chap5/Stream.scala
@@ -1,9 +1,16 @@
 package chap5
 
+import chap5.Stream.{cons, empty}
+
 sealed trait Stream[+A] {
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, t) => Some(h())
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
   }
 
   /** Exercise 5.1 */
@@ -16,7 +23,7 @@ sealed trait Stream[+A] {
   def take(n: Int): Stream[A] = (this, n) match {
     case (_, n) if n <= 0 => Empty
     case (Empty, _) => Empty
-    case (Cons(h, t), n) => Cons(h, () => t().take(n - 1))
+    case (Cons(h, t), n) => cons(h(), t().take(n - 1))
   }
 
   def drop(n: Int): Stream[A] = (this, n) match {
@@ -28,8 +35,42 @@ sealed trait Stream[+A] {
   /** Exercise 5.3 */
   def takeWhile(p: A => Boolean): Stream[A] = this match {
     case Empty => Empty
-    case Cons(h, t) if p(h()) => Cons(h, () => t().takeWhile(p))
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
     case Cons(_, t) => t().takeWhile(p)
+  }
+
+  /** Exercise 5.4 */
+  def forAll(p: A => Boolean): Boolean = this match {
+    case Empty => false
+    case Cons(h, t) if t() == Empty => p(h())
+    case Cons(h, t) => p(h()) && t().forAll(p)
+  }
+
+  /** Exercise 5.5 */
+  def takeWhile2(p: A => Boolean): Stream[A] = {
+    foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else b)
+  }
+
+  /** Exercise 5.6 */
+  def headOption2: Option[A] = {
+    foldRight(None: Option[A])((a, _) => Some(a))
+  }
+
+  /** Exercise 5.7 */
+  def map[B](f: A => B): Stream[B] = {
+    foldRight(empty[B])((a, b) => cons(f(a), b))
+  }
+
+  def filter(f: A => Boolean): Stream[A] = {
+    foldRight(empty[A])((a, b) => if (f(a)) cons(a, b) else b)
+  }
+
+  def append[B >: A](z: => B): Stream[B] = {
+    foldRight(Stream(z))((a, b) => cons(a, b))
+  }
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = {
+    foldRight(empty[B])((a, b) => f(a).foldRight(b)((a, b) => cons(a, b)))
   }
 }
 
--- src/test/scala/chap5/StreamTest.scala
+++ src/test/scala/chap5/StreamTest.scala
@@ -35,4 +35,52 @@ class StreamTest extends AnyFlatSpec {
     Stream(-1, 0, 1, 2).takeWhile(isPositive).toList shouldEqual List(1, 2)
     Stream(-1, 0).takeWhile(isPositive) shouldEqual Empty
   }
+
+  it should "forAll" in {
+    val isPositive = (x: Int) => x > 0
+    Empty.forAll(isPositive) shouldEqual false
+    Stream(1).forAll(isPositive) shouldEqual true
+    Stream(-1).forAll(isPositive) shouldEqual false
+    Stream(1, 2, 3).forAll(isPositive) shouldEqual true
+    Stream(-1, 2, 3).forAll(isPositive) shouldEqual false
+  }
+
+  it should "takeWhile2" in {
+    val isPositive = (x: Int) => x > 0
+    Empty.takeWhile2(isPositive) shouldEqual Empty
+    Stream(1, 2).takeWhile2(isPositive).toList shouldEqual List(1, 2)
+    Stream(-1, 0, 1, 2).takeWhile2(isPositive).toList shouldEqual List(1, 2)
+    Stream(-1, 0).takeWhile2(isPositive) shouldEqual Empty
+  }
+
+  it should "headOption2" in {
+    Empty.headOption2 shouldEqual None
+    Stream(1, 2).headOption2 shouldEqual Some(1)
+    Stream(1).headOption2 shouldEqual Some(1)
+  }
+
+  it should "map" in {
+    val addOne = (a: Int) => a + 1
+    Empty.map(addOne) shouldEqual Empty
+    Stream(1, 2).map(addOne).toList shouldEqual Stream(2, 3).toList
+  }
+
+  it should "filter" in {
+    val isPositive = (x: Int) => x > 0
+    Empty.filter(isPositive) shouldEqual Empty
+    Stream(1, 2).filter(isPositive).toList shouldEqual List(1, 2)
+    Stream(-1, 0, 1, 2).filter(isPositive).toList shouldEqual List(1, 2)
+    Stream(-1, 0).filter(isPositive) shouldEqual Empty
+  }
+
+  it should "append" in {
+    Stream.empty[Int].append(1).toList shouldEqual Stream(1).toList
+    Stream(1, 2).append(3).toList shouldEqual Stream(1, 2, 3).toList
+  }
+
+  it should "flatMap" in {
+    val toStream = (a: Int) => Stream(a, a)
+    Stream.empty[Int].flatMap(toStream) shouldEqual Empty
+    Stream(1, 2).flatMap(toStream).toList shouldEqual Stream(1, 1, 2, 2).toList
+  }
 }
